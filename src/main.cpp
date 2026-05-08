8b3f5867f1ce52bf24d95ab8eebf95cd64bc60c1
Mark Golovatskikh <m.golovatskih@gmail.com>
2026-05-08 19:08:27 +0200

    prepare

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Triangle", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -26,10 +26,9 @@ int main()
         return -1;
     }
 
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // red
-
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.5f, 0.6f, 0.8f, 1.0f); // background
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
