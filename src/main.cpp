ae5112af372817acae9d62c3e4097140af3baef7
Mark Golovatskikh <m.golovatskih@gmail.com>
2026-05-09 22:32:11 +0200

    awesomeface container

--- src/main.cpp
+++ src/main.cpp
@@ -26,32 +26,58 @@ int main() {
     std::cout << "Failed to initialize GLAD\n";
     return -1;
   }
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
-  // set the texture wrapping/filtering options (on the currently bound texture
-  // object)
+  unsigned int textures[2];
+  auto &[texture1, texture2] = textures;
+  glGenTextures(2, textures);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+  // set the texture1 wrapping/filtering options (on the currently bound
+  // texture1 object)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // load and generate the texture
-  int width, height, nrChannels;
-  unsigned char *data = stbi_load("assets/textures/container.jpg", &width,
-                                  &height, &nrChannels, 0);
-  if (data) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-                 GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-  } else {
-    std::cout << "Failed to load texture" << std::endl;
+  // load and generate the texture1
+  {
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("assets/textures/container.jpg", &width,
+                                    &height, &nrChannels, 0);
+    if (data) {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                   GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+      std::cout << "Failed to load texture1" << std::endl;
+    }
+    stbi_image_free(data);
   }
-  stbi_image_free(data);
-
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  // set the texture2 wrapping/filtering options (on the currently bound
+  // texture2 object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load and generate the texture2
+  {
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("assets/textures/awesomeface.png", &width,
+                                    &height, &nrChannels, 0);
+    stbi_set_flip_vertically_on_load(false);
+    if (data) {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA,
+                   GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+      std::cout << "Failed to load texture1" << std::endl;
+    }
+    stbi_image_free(data);
+  }
   Shader shader("assets/shaders/shader.vert", "assets/shaders/shader.frag");
   float vertices[] = {
-      // positions        // colors         // texture coords
+      // positions        // colors         // texture1 coords
       0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
       0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
@@ -80,12 +106,18 @@ int main() {
   glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                         (void *)(6 * sizeof(float)));
   glEnableVertexAttribArray(2);
+  shader.use();
+  shader.setInt("texture1", 0);
+  shader.setInt("texture2", 1);
 
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.5f, 0.6f, 0.8f, 1.0f); // background
     glClear(GL_COLOR_BUFFER_BIT);
 
-    shader.use();
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
