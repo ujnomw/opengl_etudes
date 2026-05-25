8dc9c87200e6ec39441df69e25f1622c4ca0cb7b
Mark Golovatskikh <m.golovatskih@gmail.com>
2026-05-25 22:59:16 +0200

    cubes scattered around

--- src/main.cpp
+++ src/main.cpp
@@ -123,6 +123,18 @@ int main() {
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+};
   // clang-format on
   unsigned int VBO, VAO, EBO;
   glGenBuffers(1, &VBO);
@@ -150,25 +162,29 @@ int main() {
     glBindTexture(GL_TEXTURE_2D, texture2);
     glBindVertexArray(VAO);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f),
-                        glm::vec3(0.5f, 1.0f, 0.0f));
     glm::mat4 view = glm::mat4(1.0f);
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
     glm::mat4 perspective = glm::mat4(1.0f);
     perspective =
-        glm::perspective(glm::radians(80.0f), 800.f / 600.f, 0.1f, 100.0f);
+        glm::perspective(glm::radians(45.0f), 800.f / 600.f, 0.1f, 100.0f);
 
-    int modelLoc = glGetUniformLocation(shader.ID, "model");
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     int viewLoc = glGetUniformLocation(shader.ID, "view");
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
     int perspectiveLoc = glGetUniformLocation(shader.ID, "perspective");
     glUniformMatrix4fv(perspectiveLoc, 1, GL_FALSE,
                        glm::value_ptr(perspective));
-    glDrawArrays(GL_TRIANGLES, 0, 36);
 
+    for (size_t i = 0; const auto &pos : cubePositions) {
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, pos);
+      float angle = glm::radians(20.f * i);
+      model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+      int modelLoc = glGetUniformLocation(shader.ID, "model");
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+      ++i;
+    }
     // glm::mat4 trans2 = glm::mat4(1.0f);
     // trans2 = glm::translate(trans2, glm::vec3(-0.5f, 0.5f, 0.0f));
     // float scaleValue = 1.f + sin(glfwGetTime()) / 2.3f;
